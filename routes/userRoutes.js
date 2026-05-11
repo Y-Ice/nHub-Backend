a2f8496eb3f2c201f92deb47812c4915212e9703
Y-Ice <isaacyakubu544@gmail.com>
2026-05-11 15:15:40 +0100

    Added delete and update to our code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -56,7 +56,12 @@ return res.json({
 
 
 const users = getUser();
-const newUser = { name, age, email }; 
+const newUser = {
+    id:Date.now(),
+    ...req.body
+    }; 
+
+
 // Add the new user to the users array and save it to the file
 users.push(newUser);
 saveUser(users);
@@ -74,6 +79,60 @@ router.get('/', (req, res) => {
 const users = getUser();
 res.json(users);
 }); 
+
+
+// Update Users
+router.patch('/:id', (req, res) => {
+    // get user id from file
+    const users = getUser();
+    // get user from url
+    const id = Number(req.params.id);
+    // find user by id
+    const userIndex = users.findIndex(user => user.id === id);
+
+    // check if user does not exist
+    if (userIndex === -1) {
+        return res.json({
+            message: 'User not found'
+        });
+    }
+    // update user data
+    users[userIndex] = {
+        // keep existing data and update with new data from request body
+        ...users[userIndex],
+        // return all data from request body and overwrite existing data with same keys
+        ...req.body
+    };
+    // save updated users to file
+    saveUser(users);
+    res.json({
+        message: 'User updated successfully',
+        user: users[userIndex]
+    });
+});
+
+// Delete user
+router.delete('/:id', (req, res) => {
+    const users = getUser();
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex(user => user.id === id);
+
+    // check if user does not exist
+    if (userIndex === -1) {
+        return res.json({
+            message: 'User not found'
+        });
+    }
+
+    // remove user from array
+    const deletedUser = users.splice(userIndex, 1)[0];
+    // save updated users to file
+    saveUser(users);
+    res.json({
+        message: 'User deleted successfully',
+        user: deletedUser
+    });
+});
 
 // this is use top export the router to be used in other files, such as server.js
 module.exports = router;
